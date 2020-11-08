bd6afb7e3b350f3bfe5387a49c0de1d2bf659eb5
JAVIER ALEXANDER HERNANDEZ LEON <hernandezjavl@HILDEBOG.LOCAL>
2020-11-08 17:26:25 -0500

    Petición GET http

--- MuziekFy/src/app/components/home/home.component.ts
+++ MuziekFy/src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor( private http: HttpClient ) {
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .subscribe( (respuesta: any) => { 
+        console.log(respuesta);
+        this.paises = respuesta;
+      });
+   }
 
   ngOnInit(): void {
   }
